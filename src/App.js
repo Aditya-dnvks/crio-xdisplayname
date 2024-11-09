d1d47b817224bdb3a1daa19fcccd88fa54daf437
Aditya-dnvks <aditya.dnvks@gmail.com>
2024-11-10 00:53:22 +0530

    x display name assignment

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.name === "firstName") {
+      setFirstName(e.target.value);
+    } else {
+      setLastName(e.target.value);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(`${firstName} ${lastName}`);
+  };
+  return (
+    <div style={{ margin: 30 }}>
+      <h1 style={{ margin: 10 }} s>
+        Full Name Display
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label style={{ margin: 10 }}>First Name:</label>
+          <input
+            required
+            type="text"
+            name="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label style={{ margin: 10 }}>Last Name:</label>
+          <input required type="text" name="lastName" onChange={handleChange} />
+        </div>
+
+        <button type="submit" style={{ margin: 10 }}>
+          Submit
+        </button>
+      </form>
+      <p style={{ margin: 10 }}>{message}</p>
+    </div>
+  );
+}
+
+export default App;
